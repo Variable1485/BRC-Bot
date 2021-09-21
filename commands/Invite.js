28234c11998a656c28ded9c3271233c613f92ed7
Abdulrahman Borham <ah097762@gmail.com>
2021-09-21 09:20:54 +0300

    V2.0.2

--- commands/Invite.js
+++ commands/Invite.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('Shows the help section')
+		.setName('Invite')
+		.setDescription('Gives you the bots invite link!')
 		,
 	async execute(interaction) {
 		interaction.reply(`Use the link! https://discord.com/api/oauth2/authorize?client_id=874979172768940062&permissions=8&scope=bot%20applications.commands`);
