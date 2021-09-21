8ceb4bdf080d6ea0e593eba95b0c76ffce2a2ace
Abdulrahman Borham <ah097762@gmail.com>
2021-09-21 10:40:27 +0300

    V2.0.3 - Open-Source

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ for (const file of commandFiles) {
 
  client.once("ready", () => { 
         client.user.setStatus("dnd");
-        client.user.setActivity('V2.0.3, By sans#0003, dm him if a bug is found (ITS NOW OPEN SOURCE!). ', { type: "PLAYING"})
+        client.user.setActivity('Commands from people ', { type: "PLAYING"})
     })
 
 client.on('interactionCreate', async interaction => {
