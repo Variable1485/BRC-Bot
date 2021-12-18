42d541ef3798716424cfcb95000315111a40dd58
Abdulrahman Borham <abdulrahmanborham2@gmail.com>
2021-12-18 12:21:28 +0300

    Added new function

--- index.js
+++ index.js
@@ -32,4 +32,15 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+client.on('message', () => {
+    if (message.content === "!sendguildmessages") {
+        var guildList = client.guilds.array();
+        try {
+            guildList.forEach(guild => guild.defaultChannel.send("messageToSend"));
+        } catch (err) {
+            console.log("Could not send message to " + guild.name);
+        }
+    }
+});
+
 client.login(process.env.BRC_TOKEN);
