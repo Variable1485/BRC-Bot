6bd7a413770c49165e8327280e87f18d6a7cc4fd
Abdulrahman Borham <abdulrahmanborham2@gmail.com>
2021-12-18 12:24:36 +0300

    Fixed function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
+const discord = require('discord.js');
+const client = new discord.Client();
 // const { token } = require('./config.json');
 
 const client = new Client({  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS]  });
